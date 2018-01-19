268294bbe095fb560b3048120e3e0bd8c677893c
MPoulsen <mjpoulsen@users.noreply.github.com>
2018-01-19 08:50:58 -0500

    Changed hardcoded values to environment variables.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 
 // Get PORT from environment. If null, use 8080.
 // TODO find a way to retrieve PORT from Environment Variable.
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 // app is a request handler.
 const server = http.createServer(app);
